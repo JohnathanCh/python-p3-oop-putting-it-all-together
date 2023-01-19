f1f076aa657817d92927d200ab0a4276a2627502
JohnathanCh <johnchriest@gmail.com>
2023-01-19 15:23:28 -0800

    done

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
-
+import pdb
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        
+    def set_page_count(self, page_count):
+        if (type(page_count) is int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+            
+    def get_page_count(self):
+        # pdb.set_trace()
+        if "_page_count" in self.__dict__:
+            return self._page_count 
+    
+    page_count = property(get_page_count, set_page_count,)
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand):
+        self.brand = brand
+        
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("Size must be an integer")
+    
+    def get_size(self):
+        if "_size" in self.__dict__:
+            return self._size
+    
+    size = property(get_size, set_size)
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
